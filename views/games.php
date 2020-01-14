a0850870e46c38ef5495e59b3b89ab12bf7ff4ba
Max <maxime.leszczynski@laposte.net>
2020-01-14 12:57:09 +0100

    Commit3

--- views/games.php
+++ views/games.php
@@ -1,13 +1,13 @@
 <?php $title = "Teams"; ?>
 <?php ob_start(); ?>
 
-<h1 class="mt-3"style="text-transform: uppercase;text-align:center;color:blue;"><img style="text-align:center;"src="public/l1.png" alt="L1"class="mb-3"><br>Liste de jeux vidéos</h1>
+<h1 class="mt-3"style="text-transform: uppercase;text-align:center;color:blue;"><br>Liste de jeux vidéos</h1>
 
 <div class="container">
     <div class="row">
         <?php foreach ($listGame as $game) { ?>
             <div class="card mt-3 col-6" style="width: 18rem;">
-                <img class="card-img-top" style="height: 400px;weight:50px;"src="<?php echo $game['cover']; ?>" alt="Card image cap">
+                <img class="card-img-top" style="height: 300px;weight:50px;"src="<?php echo $game['cover']; ?>" alt="Card image cap">
             </div>
             <div class="card-body col-6">
                 <p class="card-title "style="text-align:center;">
@@ -16,16 +16,14 @@
                     </a>
                 </p>
                 <p class="card-text col-6 ">
-                    <p>Description :<?php echo $game['description']; ?></p>
-                    <p>Classification :<?php echo $game['classification']; ?></p>
-                    <p>Date de sortie :<?php echo $game['date']; ?></p>
-                    <p>Lien :<?php echo $game['link']; ?></p>
+                    <p><?php echo $game['description']; ?></p>
+                    <p style="font-weight: bold;">Sortie :<?php echo $game['date']; ?></p>
                     <a href="?route=matchs&id=<?php echo $game['id']; ?>" class="btn btn-primary">Jeux</a>
-                    <p class ="card "style="color:green;font-weight: bold;"><?php echo $game['review']; ?>/20</p>
                 </p>
 
 
             </div>
+            <p class =" card col-12 mt-3"style="color:green;background-color: lightgrey;font-weight: bold;text-align:center;"><?php echo $game['review']; ?>/20</p>
 
         <?php } ?>
     </div>
